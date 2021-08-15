87cac20c7e654a80be44279e1d741c45140b5fbc
Guillaume Bédard <bgui34@gmail.com>
2021-08-15 13:09:24 -0400

    Navigation changes

--- src/navigations/BottomNavigator.jsx
+++ src/navigations/BottomNavigator.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from '@react-navigation/native';
-import RoutineNavigator from './RoutineNavigator';
-import Pomodoro from '../screens/Pomodoro/Pomodoro';
-import Mantra from '../screens/Mantra/Mantra';
+import { NavigationContainer } from "@react-navigation/native";
+import RoutineNavigator from "./RoutineNavigator";
+import Pomodoro from "../screens/Pomodoro/Pomodoro";
+import Mantra from "../screens/Mantra/Mantra";
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 export default function BottomNavigator() {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator screenOptions={{headerStyle: {backgroundColor: '#08FF7A'}}}>
-                <Tab.Screen name='Routine' component={RoutineNavigator} options={{headerShown: false}} />
-                <Tab.Screen name='Pomodoro' component={Pomodoro} />
-                <Tab.Screen name='Mantra' component={Mantra} />
-            </Tab.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#08FF7A" },
+          headerTintColor: "white",
+        }}
+      >
+        <Tab.Screen
+          name="Routine"
+          component={RoutineNavigator}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen name="Pomodoro" component={Pomodoro} />
+        <Tab.Screen name="Mantra" component={Mantra} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
--- src/navigations/RoutineNavigator.jsx
+++ src/navigations/RoutineNavigator.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import { createStackNavigator } from '@react-navigation/stack';
-import Routine from '../screens/Routine/Routine';
-import AddRoutine from '../screens/Routine/AddRoutine/AddRoutine';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Routine from "../screens/Routine/Routine";
+import AddRoutine from "../screens/Routine/AddRoutine/AddRoutine";
+import { Ionicons } from "@expo/vector-icons";
+import { Icon, Text, Button, Logo } from "native-base";
 
-const RoutineStack = createStackNavigator()
+const RoutineStack = createStackNavigator();
 
 export default function RoutineNavigator() {
-    return (
-        <RoutineStack.Navigator screenOptions={{headerStyle: {backgroundColor: '#08FF7A'}, headerTintColor: 'white',}} >
-            <RoutineStack.Screen name='Routine' component={Routine} />
-            <RoutineStack.Screen name='AddRoutine' component={AddRoutine} />
-        </RoutineStack.Navigator>
-    )
+  return (
+    <RoutineStack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#08FF7A" },
+        headerTintColor: "white",
+      }}
+    >
+      <RoutineStack.Screen
+        name="Routine"
+        component={Routine}
+        options={({ navigation }) => ({
+          headerRight: () => (
+            <Icon
+              color="#fff"
+              right={5}
+              onPress={() => {
+                navigation.push("AddRoutine");
+              }}
+              as={<Ionicons name="add-outline" />}
+            />
+          ),
+        })}
+      />
+      <RoutineStack.Screen name="AddRoutine" component={AddRoutine} />
+    </RoutineStack.Navigator>
+  );
 }
