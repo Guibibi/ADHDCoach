beb9ee8e8c1dcda4a1a0dc7e8be3593e5cd97961
Guillaume Bédard <bgui34@gmail.com>
2021-08-21 00:53:49 -0400

    Added icons for navigation

--- src/navigations/BottomNavigator.tsx
+++ src/navigations/BottomNavigator.tsx
@@ -5,6 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import RoutineNavigator from "./RoutineNavigator";
 import Pomodoro from "../screens/Pomodoro/Pomodoro";
 import Mantra from "../screens/Mantra/Mantra";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -18,12 +19,34 @@ export default function BottomNavigator() {
         }}
       >
         <Tab.Screen
-          name="Routine"
+          name="RoutineNavigator"
           component={RoutineNavigator}
-          options={{ headerShown: false }}
+          options={{
+            headerShown: false,
+            title: "Routine",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="time-outline" color={color} size={size} />
+            ),
+          }}
         />
-        <Tab.Screen name="Pomodoro" component={Pomodoro} />
-        <Tab.Screen name="Mantra" component={Mantra} />
+        <Tab.Screen
+          name="Pomodoro"
+          component={Pomodoro}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="timer-outline" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Mantra"
+          component={Mantra}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="ribbon-outline" color={color} size={size} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
