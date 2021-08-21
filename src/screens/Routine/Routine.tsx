ffe5a81b5f033f0714acc2938fd6166764a44ecd
Guillaume Bédard <bgui34@gmail.com>
2021-08-21 01:26:44 -0400

    Styling and animation for routine

--- src/screens/Routine/Routine.tsx
+++ src/screens/Routine/Routine.tsx
@@ -10,6 +10,7 @@ import {
 } from "native-base";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useIsFocused } from "@react-navigation/core";
+import * as Animatable from "react-native-animatable";
 
 export default function Routine({ navigation }) {
   const [tasks, setTasks] = useState(null);
@@ -28,38 +29,37 @@ export default function Routine({ navigation }) {
 
   useEffect(() => {
     getData();
-    console.log("====================================");
-    console.log("docused");
-    console.log("====================================");
   }, [isFocused]);
 
   return (
-    <Box h="100%" bg="coolGray.300">
+    <Box h="100%" px={4} pt={2} bg="coolGray.500">
       <FlatList
         data={tasks}
         renderItem={({ item }) => (
-          <Box px={0} py={4} mb={0} bg="coolGray.100">
-            <Flex
-              direction="row"
-              justifyContent="space-around"
-              alignItems="center"
-            >
-              <VStack w={10}>
-                <Center>
-                  <Text fontSize="lg">{item.streak}</Text>
-                  <Text fontSize="xs">days</Text>
-                </Center>
-              </VStack>
+          <Animatable.View animation="bounceIn">
+            <Box px={0} py={4} borderRadius="lg" my={2} bg="white">
+              <Flex
+                direction="row"
+                justifyContent="space-around"
+                alignItems="center"
+              >
+                <VStack w={10}>
+                  <Center>
+                    <Text fontSize="lg">{item.streak}</Text>
+                    <Text fontSize="xs">days</Text>
+                  </Center>
+                </VStack>
 
-              <VStack w={200}>
-                <Center>
-                  <Text fontSize="xl">{item.title}</Text>
-                  <Text fontSize="xs">{item.alarmTime}</Text>
-                </Center>
-              </VStack>
-              <Checkbox value="one" aria-label="Done" w={10}></Checkbox>
-            </Flex>
-          </Box>
+                <VStack w={200}>
+                  <Center>
+                    <Text fontSize="xl">{item.title}</Text>
+                    <Text fontSize="xs">{item.alarmTime}</Text>
+                  </Center>
+                </VStack>
+                <Checkbox value="one" aria-label="Done" w={10}></Checkbox>
+              </Flex>
+            </Box>
+          </Animatable.View>
         )}
         keyExtractor={(item) => item.id}
       />
